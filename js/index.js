bc0b2f395f565e96084c472dca31b8a1bf281185
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-07 11:00:03 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,79 @@
+const inquirer = require("inquirer");
+const fs = require("fs"); 
+
+const generateReadme = (answers) => 
+    `# ${answers.title}
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License 
+${answers.license}
+
+## How to Contribute
+${answers.contributing}
+
+## Tests
+${answers.tests}`;
+
+
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },    
+    {
+        type: 'input',
+        message: 'Title: What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Description: Write a short description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Installation: Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage: Provide instructions and examples for use.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'License: Select the license which covers your project.',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Contributing: Include guidelines for how to contribute, if applicable.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Tests: Provide instructions on how to run tests on your application, if applicable.',
+        name: 'tests',
+    },
+  ])
+  .then((response) => {
+    const errorFunction = (err) => err ? console.error(err) : console.log('Commit logged!');
+    fs.writeFile('./portfolio/index.html', generateReadme(response), errorFunction);
+  }
+  );
+
